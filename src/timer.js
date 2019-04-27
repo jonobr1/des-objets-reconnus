5393c3191c0818b183a405fb14eca6957e879133
Jono <developer@jono.fyi>
2019-04-27 18:05:01 +0200

    Image Animations Working with Cloud Storage Based ML Process

---
+++ src/timer.js
@@ -0,0 +1,22 @@
+
+var timer = {
+
+  clock: TWEEN,
+
+  now: Date.now,
+
+  interval: 1000,
+
+  // One Day Resolution
+  resolution: 1000 * 60 * 60 * 24,
+
+  // Get an index based on day of the year.
+  getIndex: function(limit) {
+    var index = Math.floor(timer.now() / timer.resolution);
+    if (limit) {
+      index = index % limit;
+    }
+    return index;
+  }
+
+};
